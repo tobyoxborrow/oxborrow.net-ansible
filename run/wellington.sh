ca8830dea1c126ad9dcf75353879b0fb752be889
Toby Oxborrow <toby@oxborrow.net>
2019-03-14 10:57:43 +0800

    Syntax check helpers
    Add some scripts to help check the syntax.
    
    * checks/check.sh can be run as part of CI/CD for automated gating
    * Added check parameters to the run/ scripts so they can be run without
      making changes, useful to see what may happen before actually doing it.

--- run/wellington.sh
+++ run/wellington.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
+CHECK_ARGS=""
+if [[ $1 == "check" ]]; then
+    CHECK_ARGS="--check"
+    echo "CHECK MODE"
+fi
+
 ansible-playbook --diff --inventory ./ansible/inventory \
+    ${CHECK_ARGS} \
     --ask-become-pass \
     --vault-password-file=<(/usr/local/bin/pass oxborrow.net/ansible/vault) \
     ./ansible/mailservers.yml --limit "wellington"
