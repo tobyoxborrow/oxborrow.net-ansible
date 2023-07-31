98e101fb10c638c37da874b987fd8f5af655982b
Toby Oxborrow <toby@oxborrow.net>
2023-08-01 00:25:59 +0900

    Minecraft ansible role
    Working version.

---
+++ run/minecraft.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+CHECK_ARGS=""
+if [[ $1 == "check" ]]; then
+    CHECK_ARGS="--check"
+    echo "CHECK MODE"
+fi
+
+ansible-playbook \
+    --diff \
+    --inventory ./ansible/inventory \
+    ${CHECK_ARGS} \
+    --vault-id=run/vault-pass-client.py \
+    ./ansible/home.oxborrow.net.yml --limit "minecraft"
